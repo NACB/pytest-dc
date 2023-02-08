a22a2d402d178d9aff1428629d9008fe4bf542dd
Nathanael Baumann <nbaumann24@cmc.edu>
2023-02-07 17:26:54 -0800

    fixed a python grammar error that didn't show up on the linter

--- Fixme.py
+++ Fixme.py
@@ -5,5 +5,5 @@ def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
     '''
-    n = list(range(n+1))
+    n = list(range(n + 1))
     return list(filter(lambda x: x % 2 == 0, n))
